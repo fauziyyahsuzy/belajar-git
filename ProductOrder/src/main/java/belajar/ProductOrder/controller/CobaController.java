8a76ed560e64e49e4f52e2c23693ef4844305956
Fauziyyah <fauziyyahsuzy@gmail.com>
2023-05-25 15:42:52 +0700

    menambahkan revisi

--- ProductOrder/src/main/java/belajar/ProductOrder/controller/CobaController.java
+++ ProductOrder/src/main/java/belajar/ProductOrder/controller/CobaController.java
@@ -1,24 +1,8 @@
 package belajar.ProductOrder.controller;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import belajar.ProductOrder.repository.CategoryRepository;
-import belajar.ProductOrder.Dto.ProductDto;
-import belajar.ProductOrder.repository.CategoryRepository;
-import belajar.ProductOrder.repository.ProductRepository;
 
-import belajar.ProductOrder.repository.*;
-
-@RestController
-@RequestMapping("/coba")
 public class CobaController {
 	
-	@Autowired
-	private CategoryRepository categoryRepository;
-	
-	ModelMapper modelMapper = new ModelMapper();
-	
+	//sdjsdkjas
 }
